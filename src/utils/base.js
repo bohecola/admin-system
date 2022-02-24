715b50305690e95a3ab4a9e5b88aa00b63f2bf0b
bohecola <bohecola@outlook.com>
2022-02-24 12:53:51 +0800

    devURL

--- src/utils/base.js
+++ src/utils/base.js
@@ -1,6 +1,6 @@
 const prod = 'https://canday.site:3000/api';
 
-const dev = 'http://localhost:3000/api';
+const dev = 'http://localhost:3001/api';
 
 const base = process.env.NODE_ENV === 'production' ? prod : dev
 
